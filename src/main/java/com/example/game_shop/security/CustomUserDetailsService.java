e23fecff9fc4dcd2d3f084f2dd16e870848468ce
lh991117 <92137419+lh991117@users.noreply.github.com>
2025-12-11 18:03:51 +0900

    feat : UserDetails

---
+++ src/main/java/com/example/game_shop/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.example.game_shop.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.game_shop.user.domain.User;
+import com.example.game_shop.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+
+        return new CustomUserDetails(user);
+    }
+
+}
--- src/main/java/com/example/game_shop/user/repository/UserRepository.java
+++ src/main/java/com/example/game_shop/user/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.example.game_shop.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.game_shop.user.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
     boolean existsByEmail(String email);
 }
